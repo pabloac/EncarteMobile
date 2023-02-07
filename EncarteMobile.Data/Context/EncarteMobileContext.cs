cf91c17158ce922af3637d2efb77442654ab6fc9
Pablo Cardoso <pabloac.ti@gmail.com>
2023-02-07 16:30:25 -0300

    Atualizando estrutura do projeto inicial

---
+++ EncarteMobile.Data/Context/EncarteMobileContext.cs
@@ -0,0 +1,40 @@
+﻿using EncarteMobile.Data.Mappings;
+using EncarteMobile.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncarteMobile.Data.Context
+{
+    public class EncarteMobileContext: DbContext
+    {
+        private readonly string _connectionString;
+
+        public EncarteMobileContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+
+        #region "DBSETS"
+
+        public DbSet<User> Users { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+            base.OnModelCreating(modelBuilder); 
+        }
+
+    }
+}
---
+++ EncarteMobile.Data/Mappings/UserMap.cs
@@ -0,0 +1,20 @@
+﻿using EncarteMobile.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncarteMobile.Data.Mappings
+{
+    public class UserMap: IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(100);
+        }
+    }
+}
